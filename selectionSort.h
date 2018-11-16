2f715621b665b5af9f154807d3d4fdea7584b6c2
Joao Victor <jvmazagao@live.com>
2018-11-16 19:19:25 -0200

    Alteração do arquivo de selection sort para adequação ao codigo

--- selectionSort.h
+++ selectionSort.h
@@ -1,23 +1,31 @@
 //
 // Created by jvmazagao on 14/11/18.
 //
-#include "typeCode.h"
+#include <stdio.h>
+#include "item.h"
+
 #ifndef TP_AEDS1_SELECTIONSORT_H
 #define TP_AEDS1_SELECTIONSORT_H
 
-#endif //TP_AEDS1_SELECTIONSORT_H
 
-void Selecao(TipoItem ∗A, TipoIndice n){
-    TipoIndice i , j , Min;
+
+void Selecao(TipoItem *A, int n){
+    int i;
+    int j;
+    int min;
+
     TipoItem x;
 
-    for ( i = 1; i <= n−1; i++){
-        Min = i ;
-        for ( j = i + 1; j <= n; j++)
-            if (A[ j ] .Chave < A[Min] .Chave)
-                Min = j;
-                x = A[Min];
-                A[Min] = A[ i ];
-                A[ i ] = x;
+    for (i = 1; i <= n -1; i++){
+        min = i;
+        for (j = i+1; j < n ; j++) {
+            if(A[i].Chave < A[min].Chave)
+                min = j;
+            x = A[min];
+            A[min] = A[i];
+            A[i] = x;
+        }
     }
 }
+
+#endif //TP_AEDS1_SELECTIONSORT_H
