be4aeedf79d946def7d8d1e4ef7c56733752324b
Joao Victor <jvmazagao@live.com>
2018-11-17 17:55:32 -0200

    Insertion sort funcionando, e pronto pra uso.

--- isertionSort.h
+++ isertionSort.h
@@ -1,8 +1,37 @@
-//
-// Created by jvmazagao on 14/11/18.
-//
+/************************************************
+LAED1 - Trabalho Pratico 2
+Alunos:Christian Medeiros Cantarino
+       João Victor Duarte Mazagão Rodrigues
+Matricula:201722040130
+          201722040378
+Descricao do programa:Arquivo principal aonde os algoritmos serão executados
+Data:17/11/2018
+************************************************/
 
+#include "item.h"
+#include <stdio.h>
 #ifndef TP_AEDS1_ISERTIONSORT_H
 #define TP_AEDS1_ISERTIONSORT_H
+
+/*
+ * codigo adaptado do site geeks for geeks link abaixo
+ *  https://www.geeksforgeeks.org/insertion-sort/
+ */
+
+void Insercao (TipoItem * a, int n){
+    TipoItem x;
+    int i = 0, j = 0;
+
+    for (i = 1; i <= n - 1; i++) {
+        x = a[i];
+        j = i -1;
+        while(j>=0 && a[j].Chave > x.Chave){
+            a[j+1] = a[j];
+            j = j -1;
+        }
+
+        a[j+1] = x;
+    }
+}
 
 #endif //TP_AEDS1_ISERTIONSORT_H
