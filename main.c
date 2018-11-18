3c0eaadb9c005e2130759bfdf9467ba6d4c9f422
Christian <christian.cantarino@gmail.com>
2018-11-18 18:34:47 -0200

    Add medirTempo.h e biblioteca na main

--- main.c
+++ main.c
@@ -15,14 +15,18 @@ Data:15/11/2018
 #include "isertionSort.h"
 #include "mergeSort.h"
 #include "heapSort.h"
+#include "medirTempo.h"
 
 int main() {
     TipoItem * A;
+    medeTempo tempo;
     int n;
+    inicia(tempo);
     printf("Digite quantos itens deseja em seu arranjo:");
     scanf("%d",&n);
     A = geraItensAleatorios(n);
     heapSort(A, n);
     Imprime(A,n);
+    printTempo(tempo);
     return 0;
 }
